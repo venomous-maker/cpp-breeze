e96ccfa9d94c3f429055bd71407baa850e2313b0
venomous-maker <morganokumu8+1@gmail.com>
2025-12-26 11:25:59 +0300

    Implement Response::view() using the View engine

--- src/http/response.cpp
+++ src/http/response.cpp
@@ -1,5 +1,16 @@
 #include <breeze/http/response.hpp>
+#include <breeze/core/application.hpp>
+#include <breeze/support/view.hpp>
 
 namespace breeze::http {
-// Currently most logic is in the header.
+
+Response Response::view(const std::string& template_name, const nlohmann::json& data) {
+    auto view_engine = breeze::core::Application::instance().container().make<breeze::support::View>();
+    
+    Response res{StatusCode::OK};
+    res.content_type("text/html");
+    res.set_body(view_engine->render(template_name, data));
+    return res;
+}
+
 } // namespace breeze::http

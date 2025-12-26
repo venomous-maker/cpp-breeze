43cb873bc8244240ff846e039fb62d4c1ea06bb2
venomous-maker <morganokumu8+1@gmail.com>
2025-12-26 11:07:25 +0300

    Update Response implementation

--- src/http/response.cpp
+++ src/http/response.cpp
@@ -1,25 +1,5 @@
 #include <breeze/http/response.hpp>
 
-#include <sstream>
-
 namespace breeze::http {
-
-std::string Response::to_string() const
-{
-    std::ostringstream oss;
-    oss << "HTTP/1.1 " << static_cast<int>(status_);
-
-    std::string reason = Status::reason_phrase(status_);
-    if (!reason.empty()) {
-        oss << " " << reason;
-    }
-    oss << "\r\n";
-
-    for (const auto& [k, v] : headers_) {
-        oss << k << ": " << v << "\r\n";
-    }
-    oss << "\r\n" << body_;
-    return oss.str();
-}
-
+// Currently most logic is in the header.
 } // namespace breeze::http

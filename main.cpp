47559b6c023df748553a17651f73ee50b55e212a
venomous-maker <morganokumu8+1@gmail.com>
2025-12-31 23:49:10 +0300

    feat(main): streamline application initialization by consolidating service provider registration

--- main.cpp
+++ main.cpp
@@ -1,25 +1,11 @@
 #include <breeze/breeze.hpp>
-#include "app/Providers/ViewServiceProvider.hpp"
-#include "app/Providers/MiddlewareServiceProvider.hpp"
-#include "app/Providers/ControllerServiceProvider.hpp"
-
-// Forward declarations of route registration functions
-void register_web_routes(breeze::core::Application& app);
-void register_api_routes(breeze::core::Application& app);
-void register_admin_routes(breeze::core::Application& app);
+#include "app/Providers/AppServiceProvider.hpp"
 
 int main(int argc, char** argv) {
     auto app = breeze::core::Application::create();
 
-    // Register service providers
-    app->register_provider<::app::Providers::ViewServiceProvider>();
-    app->register_provider<::app::Providers::MiddlewareServiceProvider>();
-    app->register_provider<::app::Providers::ControllerServiceProvider>();
-
-    // Register routes from the routes directory
-    register_web_routes(*app);
-    register_api_routes(*app);
-    register_admin_routes(*app);
+    // Register the top-level AppServiceProvider which in turn registers other providers
+    app->register_provider<::app::Providers::AppServiceProvider>();
 
     // Default port from env or 8000
     int port = std::stoi(breeze::support::Env::get("APP_PORT", "8000"));
